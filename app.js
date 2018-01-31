51b41ba937de7a67a2fa437e13843299af3b22e3
fireArthur <thefirearthur@gmail.com>
2018-01-31 14:42:27 -0800

    Partially complete

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 // TODO
+var App = () => (
+  <div>
+    <h1>Grocery List</h1>
+    <GroceryList list={['banans', 'Peanut Butter']}/>
+  </div>
+);
+var GroceryList = (props) => (
+  <ul>
+      {props.list.map((element, index)=>{return <GroceryListItem item = {element} key = {index}/>})}
+  </ul>
+);
+
+var GroceryListItem = (props) => (
+  <div>
+    <li>{props.item}</li>
+  </div>
+);
+
+// var PeanutButter = () => (
+//   <div>
+//     <span></span>
+//   </div>
+// );
+ReactDOM.render(<App />, document.getElementById('app'));
